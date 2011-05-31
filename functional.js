a7bdf915a498b6a160b4db0368605387f1b6974d
Kevin Xiao <kevin.xiaowl@gmail.com>
2011-05-31 15:36:44 +0800

    make List works like a factory to create List object

--- functional.js
+++ functional.js
@@ -1,7 +1,12 @@
 var functional = (function(){
+    /*===============Private helper functions====================*/
     function isArray(a){
-        return a instanceof Array || (a.length && typeof a.length === "number" &&( a.length > 0 && a[0] || a.length === 0));
+        return a instanceof Array || (typeof a.length !== "undefined" && typeof a.length === "number" &&( a.length > 0 && a[0] || a.length === 0));
     }
+
+    /*===============End of private helper functions==============*/
+
+    /*===============Common public high order functions===========*/
 
     function map(list, func, arg){
         var ret = [];
@@ -15,11 +20,75 @@ var functional = (function(){
         return ret;
     }
 
+    function filter(list, func, arg){
+        var ret = [];
+        if(!isArray(list)){
+            throw "Filter function can only be applied to array or string";
+        }
+        for(var i=0, ii=list.length; i < ii; i++){
+            if(func(list[i], i, arg)){
+               ret.push(list[i]);
+            }
+        }
+        return ret;
+    }
+
+    /*================End of common high order functions=============*/
+
+    /*================data.List======================================*/
+
+    var data = (function(){
+        function List(list){
+            return new List.fn.init(list);
+        }
+
+        List.fn = List.prototype = {
+            init: function(list){
+                this.list = list;
+                this.length = list.length;
+            },
+
+            value: function(){
+                return this.list;
+            },
+
+            map: function(func, arg){
+                return new List(map(this.list, func, arg));
+            },
+
+            filter: function(func, arg){
+                return new List(filter(this.list, func, arg));
+            }
+        }
+
+        List.fn.init.prototype = List.prototype;
+
+        var data = {};
+        install(data, function(scope){
+            map("List".split("|"), function(name){
+                scope[name] = eval(name);
+            });
+        });
+
+        return  data;
+    })();
+
+    /*================End of data.List===============================*/
+
+    /*================Helper functions to setup======================*/
+
+    function install(scope, installer){
+        installer(scope);
+    }
+
+    /*================End of setup helper============================*/
 
 
     var functional = {};
-    map("map".split("|"), function(name){
-        functional[name] = eval(name);
+    install(functional, function(scope){
+        map("map|filter|data".split("|"), function(name){
+            scope[name] = eval(name);
+        });
     });
 
     return functional;
